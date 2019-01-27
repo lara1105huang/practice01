7c04aacb6d04d57241ee626234aa1c4921725443
“larahuang” <larahuang@gmail.com>
2019-01-27 19:14:35 +0800

    modify img imageUrl

--- candy/src/components/pages/Products.vue
+++ candy/src/components/pages/Products.vue
@@ -67,7 +67,7 @@
                   <input type="file" id="customFile" class="form-control"
                     ref="files" @change="uploadFile">
                 </div>
-                <img img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=828346ed697837ce808cae68d3ddc3cf&auto=format&fit=crop&w=1350&q=80"
+                <img
                   class="img-fluid" :src="tempProduct.imageUrl" alt="">
               </div>
               <div class="col-sm-8">
